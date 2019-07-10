bab69b80199a9cfd6e4ca192e35ada082db029c9
Hendri Arifin, S.Kom <cowok_cool320@yahoo.co.id>
2019-07-11 00:41:43 +0800

    base url https

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/BamegaLeague/';
-$config['base_url'] = 'http://BamegaLeague.info/';
+$config['base_url'] = 'https://BamegaLeague.info/';
 
 /*
 |--------------------------------------------------------------------------
