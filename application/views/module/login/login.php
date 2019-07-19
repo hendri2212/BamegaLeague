07266ee406fef75b82fc09c2376abe50c6e0afa4
Bintang_f <bintangfadel73@gmail.com>
2019-07-19 11:38:07 +0800

    tampilan login

--- application/views/module/login/login.php
+++ application/views/module/login/login.php
@@ -1,20 +1,22 @@
-<div class="d-flex justify-content-center">
-    <div class="card bg-dark text-light">
-        <div class="card-header">
-            <span>LOGIN ADMINISTRATOR</span>
-        </div>
-        <div class="card-body">
-            <?php echo form_open("content/cekLogin"); ?>
-                <div class="form-group">
-                    <label>Username</label>
-                    <input type="text" class="form-control" placeholder="Username" name="username"/>
-                </div>
-                <div class="form-group">
-                    <label>Password</label>
-                    <input type="password" class="form-control" placeholder="Password" name="password"/>
-                </div>
-                <button class="btn btn-danger btn-block" type="submit" name="submit">Login</button>
-            <?php echo form_close(); ?>
+<div class="row justify-content-center">
+    <div class="col-sm-12 col-md-5">
+        <div class="card bg-dark text-light mt-4 w-100">
+            <div class="card-header text-center">
+                <span>LOGIN ADMINISTRATOR</span>
+            </div>
+            <div class="card-body">
+                <?php echo form_open("content/cekLogin"); ?>
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" class="form-control" placeholder="Username" name="username"/>
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control" placeholder="Password" name="password"/>
+                    </div>
+                    <button class="btn btn-danger btn-block" type="submit" name="submit">Login</button>
+                <?php echo form_close(); ?>
+            </div>
         </div>
     </div>
 </div>
